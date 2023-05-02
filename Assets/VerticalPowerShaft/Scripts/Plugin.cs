d33c69c42cff98127c5f224168fcb87e5cbdd2fe
Tobbert <van.dijk.jordy@gmail.com>
2023-05-02 20:57:32 +0200

    Cleanup code VerticalPowerShaft

--- Assets/VerticalPowerShaft/Scripts/Plugin.cs
+++ Assets/VerticalPowerShaft/Scripts/Plugin.cs
@@ -5,32 +5,9 @@ using Timberborn.BlockObjectTools;
 
 namespace VerticalPowerShaft
 {
-    // [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-    // [BepInDependency("com.timberapi.timberapi")]
-    // public class Plugin : BaseUnityPlugin
-    // {
-    //     public const string PluginGuid = "tobbert.verticalpowershaft";
-    //     public const string PluginName = "Vertical Power Shaft";
-    //     public const string PluginVersion = "1.0.2";
-    //     
-    //     public static ManualLogSource Log;
-    //     
-    //     void Awake()
-    //     {
-    //         Log = Logger;
-    //         
-    //         Log.LogInfo($"Loaded {PluginName} Version: {PluginVersion}!");
-    //         
-    //         TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.InGame);
-    //         new Harmony(PluginGuid).PatchAll();
-    //     }
-    // }
-    
     public class Plugin : IModEntrypoint
     {
         public const string PluginGuid = "tobbert.verticalpowershaft";
-        public const string PluginName = "Vertical Power Shaft";
-        public const string PluginVersion = "1.0.2";
         
         public void Entry(IMod mod, IConsoleWriter consoleWriter)
         {
